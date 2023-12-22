00159bb9f4dd44f867be834c4060f07ec19f0ff1
dilia00 <dina_xenia@mail.ru>
2023-12-22 16:08:20 +0300

    HW_3

---
+++ HW_3/2.js
@@ -0,0 +1,47 @@
+'use strict';
+
+// "Управление списком заказов"
+
+// Реализуйте класс Order (заказ), который имеет следующие свойства и методы:
+
+// Свойство orderNumber (номер заказа) - число, уникальный номер заказа.
+// Свойство products (продукты) - массив, содержащий список продуктов в заказе.
+// Метод addProduct(product) - принимает объект product и добавляет его в список продуктов заказа.
+// Метод getTotalPrice() - возвращает общую стоимость заказа, основанную на ценах продуктов.
+
+class Order {
+    constructor(orderNumber) {
+        this.orderNumber = orderNumber;
+        this.products = [];
+        this.totalPrice = 0;
+    }
+
+    addProduct(obj) {
+        this.products.push(obj);
+    }
+
+    getTotalPrice() {
+        for (const el in this.products) {
+            this.totalPrice += this.products[el].price
+        }
+        return this.totalPrice;
+    }
+}
+
+// Пример использования класса
+class Product {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+}
+
+const order = new Order(12345);
+
+const product1 = new Product("Phone", 500);
+order.addProduct(product1);
+
+const product2 = new Product("Headphones", 100);
+order.addProduct(product2);
+
+console.log(order.getTotalPrice()); // Вывод: 600
